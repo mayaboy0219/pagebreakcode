536f42fd236e684fe27489e6d2b03592def57bea
mayaboy0219 <mayaboy0219@126.com>
2016-05-12 15:02:44 +0800

    1.0
    remove some pictiures

---
+++ lang/zh-cn.js
@@ -0,0 +1,9 @@
+﻿/*
+Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'pagebreakcode', 'zh-cn', {
+	alt: '分页符',
+	toolbar: '插入分页符',
+	subtitle: '副标题'
+} );
---
+++ plugin.js
@@ -0,0 +1,33 @@
+﻿/*
+ * PageBreakCode Plugin for CKEditor (https://github.com/mayaboy0219/pagebreakcode)
+ * Created by omdriver.com - Wang Zhuo - 11. May 2016
+ * Licensed under the terms of GPL, LGPL and MPL licenses.
+ */
+
+'use strict';
+
+(function() {
+	CKEDITOR.plugins.add( 'pagebreakcode', {
+		requires: 'fakeobjects',
+		lang: 'en,zh-cn',
+		icons: 'pagebreakcode',
+		hidpi: true,
+		init: function( editor ) {
+			if ( editor.blockless )
+				return;
+
+			editor.addCommand( 'pagebreakcode', CKEDITOR.plugins.pagebreakcodeCmd );
+
+			editor.ui.addButton && editor.ui.addButton( 'PageBreakCode', {
+				label: editor.lang.pagebreakcode.toolbar,
+				command: 'pagebreakcode'
+			});
+		}
+	} );
+
+	CKEDITOR.plugins.pagebreakcodeCmd = {
+		exec: function( editor ) {
+			editor.insertHtml("#p#" + editor.lang.pagebreakcode.subtitle + "#e#");
+		}
+	};
+})();
